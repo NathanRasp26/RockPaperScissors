3f14f72594c146eaf44baeaf612be3c26ec7c656
Rasp <26RaspN@badinhs.org>
2024-10-11 13:54:14 -0400

    completed basic function of game

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,1 +1,32 @@
 //file to program a playabe rock paper scissors game
+let choice = ['rock' , 'paper' , 'scissors']
+
+function getcomputerchoice(choice){
+    return choice[Math.floor(Math.random() * choice.length)];
+}
+console.log(getcomputerchoice(choice))
+
+function game(playerSelect, computerSelect){
+    if (playerSelect === computerSelect){
+        return "tie"
+    }
+    else if (playerSelect === "rock" && computerSelect === "scissors"){
+        return "you win";
+    }
+    else if (playerSelect === "paper" && computerSelect === "rock"){
+        return "you win";
+    }
+    else if (playerSelect === "scissor" && computerSelect === "paper"){
+        return "you win";
+    }
+    else{
+        return "you lose"
+    }
+}
+
+const playerSelect = prompt("Make your move").toLowerCase();
+const computerSelect = getcomputerchoice(choice);
+console.log(game(playerSelect , computerSelect));
+
+    
+
