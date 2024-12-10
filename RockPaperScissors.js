cbb228ac20d4b984b3367442050a597f325a8f04
Rasp <26RaspN@badinhs.org>
2024-12-09 22:20:17 -0500

    added score function

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -24,18 +24,44 @@ function game(playerSelect, computerSelect){
     }
 }
 
-let playerSelect;
-while (true) {
-    playerSelect = prompt("Make your move (rock, paper, or scissors):").toLowerCase();
-    if (choice.includes(playerSelect)) {
-        break;
-    }   else {
-        console.log("Invalid Input, select 'rock', 'paper', or 'scissors'.");
+
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+
+    while (humanScore <= 4 && computerScore <= 4) {
+        let playerSelect;
+        while (true) {
+            playerSelect = prompt("Make your move (rock, paper, or scissors):").toLowerCase();
+            if (choice.includes(playerSelect)) {
+                break;
+            } else {
+                console.log("Invalid Input, select 'rock', 'paper', or 'scissors'.");
+            }
+        }
+
+        const computerSelect = getcomputerchoice(choice);
+        console.log(`Computer Chose: ${computerSelect}`);
+        console.log(`You Chose: ${playerSelect}`);
+
+        const result = game(playerSelect, computerSelect);
+        console.log(result);
+
+        if (result === "you win") {
+            humanScore++;
+        } else if (result === "you lose") {
+            computerScore++;
+        }
+
+        console.log(`Human Score: ${humanScore}`);
+        console.log(`Computer Score: ${computerScore}`);
+    }
+
+    if (humanScore > computerScore) {
+        console.log("Congratulations! You won the game! You don't win anyting though.");
+    } else {
+        console.log("Game over! The computer won, you should be ashamed of yourself.");
     }
 }
-const computerSelect = getcomputerchoice(choice);
-console.log(`Computer Chose: ${computerSelect}`);
-console.log(game(playerSelect , computerSelect));
 
-    
-
+playGame();
